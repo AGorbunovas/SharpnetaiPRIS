f349ce44ab41cb3bcfc6063c85c2a196da46bf82
Vilija <vilijaklyve@gmail.com>
2020-06-22 12:04:10 +0300

    Login panaikinta "pakartotinai siusti laiska"

--- PRIS.WEB/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ PRIS.WEB/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -9,56 +9,56 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 
-namespace PRIS.WEB.Areas.Identity.Pages.Account
-{
-    [AllowAnonymous]
-    public class ConfirmEmailChangeModel : PageModel
-    {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+//namespace PRIS.WEB.Areas.Identity.Pages.Account
+//{
+//    [AllowAnonymous]
+//    public class ConfirmEmailChangeModel : PageModel
+//    {
+//        private readonly UserManager<IdentityUser> _userManager;
+//        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public ConfirmEmailChangeModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
+//        public ConfirmEmailChangeModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+//        {
+//            _userManager = userManager;
+//            _signInManager = signInManager;
+//        }
 
-        [TempData]
-        public string StatusMessage { get; set; }
+//        [TempData]
+//        public string StatusMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string userId, string email, string code)
-        {
-            if (userId == null || email == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
+//        public async Task<IActionResult> OnGetAsync(string userId, string email, string code)
+//        {
+//            if (userId == null || email == null || code == null)
+//            {
+//                return RedirectToPage("/Index");
+//            }
 
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Nepavyksta įkelti vartotojo, kurio ID '{userId}'.");
-            }
+//            var user = await _userManager.FindByIdAsync(userId);
+//            if (user == null)
+//            {
+//                return NotFound($"Nepavyksta įkelti vartotojo, kurio ID '{userId}'.");
+//            }
 
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ChangeEmailAsync(user, email, code);
-            if (!result.Succeeded)
-            {
-                StatusMessage = "Elektroninio pašto pakeisti nepavyko.";
-                return Page();
-            }
+//            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+//            var result = await _userManager.ChangeEmailAsync(user, email, code);
+//            if (!result.Succeeded)
+//            {
+//                StatusMessage = "Elektroninio pašto pakeisti nepavyko.";
+//                return Page();
+//            }
 
-            // In our UI email and user name are one and the same, so when we update the email
-            // we need to update the user name.
-            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
-            if (!setUserNameResult.Succeeded)
-            {
-                StatusMessage = "Vartotojo vardo pakeisti nepavyko.";
-                return Page();
-            }
+//            // In our UI email and user name are one and the same, so when we update the email
+//            // we need to update the user name.
+//            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
+//            if (!setUserNameResult.Succeeded)
+//            {
+//                StatusMessage = "Vartotojo vardo pakeisti nepavyko.";
+//                return Page();
+//            }
 
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Elektroninis paštas pakeistas sėkmingai.";
-            return Page();
-        }
-    }
-}
+//            await _signInManager.RefreshSignInAsync(user);
+//            StatusMessage = "Elektroninis paštas pakeistas sėkmingai.";
+//            return Page();
+//        }
+//    }
+//}
