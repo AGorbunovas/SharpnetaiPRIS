b1eaab84b937c10c27cf5ad3ca9b1d3b4dfdc52f
titas1111 <titasaleksandravicius@gmail.com>
2020-06-25 11:54:25 +0300

    AddTestViewModel CityName required fixed

--- PRIS.WEB/ViewModels/TestViewModels/AddTestViewModel.cs
+++ PRIS.WEB/ViewModels/TestViewModels/AddTestViewModel.cs
@@ -15,13 +15,12 @@ namespace PRIS.WEB.ViewModels.TestViewModels
 
         [Display(Name = "Miestas")]
         public City City { get; set; }
-        public List<SelectListItem> Cities { get; set; }
         
+        public List<SelectListItem> Cities { get; set; }
+
         [Required(ErrorMessage = "Įveskite miestą")]
         public string CityName { get; set; }
-
         public int TestId { get; set; }
-
         public List<Test> ListOfTests { get; set; }
     }
 }
