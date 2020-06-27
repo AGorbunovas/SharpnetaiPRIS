4d11a63f68ba1aaa3d8d175af17a81611549452f
titas1111 <titasaleksandravicius@gmail.com>
2020-06-27 16:14:12 +0300

    initial Test project setup and TestControllerShould

---
+++ PRIS.Test/Controller/TestControllerShould.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using PRIS.WEB.Controllers;
+using PRIS.WEB.Data;
+using PRIS.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PRIS.Test.Controller
+{
+    public class TestControllerShould
+    {
+        [Fact]
+        public void ReturnViewForTest()
+        {
+            var sut = new TestController(GetContextWithData());
+
+            IActionResult result = sut.Test();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        private ApplicationDbContext GetContextWithData()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.Cities.Add(new City { CityName = "Vilnius" });
+
+            return context;
+        }
+
+    }
+
+
+}
