8814129bb62ed0e7f39fab9df735602c1c8febcc
Azure Student <azure.student1@stud.kitm.lt>
2020-07-01 19:54:22 +0300

    Candidate

---
+++ PRIS.WEB/Controllers/CandidateController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PRIS.WEB.Data;
+using PRIS.WEB.Models;
+using PRIS.WEB.ViewModels.CandidateViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PRIS.WEB.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CandidateController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Candidate()
+        {
+            AddCandidateViewModel viewModel = GetViewModelWithModulesList();
+
+            return View("Candidate", viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Candidate(AddCandidateViewModel model)
+        {
+            
+            AddCandidateViewModel viewModel = GetViewModelWithModulesList();
+
+            return View(viewModel);
+        }
+
+        private AddCandidateViewModel GetViewModelWithModulesList()
+        {
+            var modules = _context.Modules.Select(x => new SelectListItem()
+            {
+                Value = x.ModuleID.ToString(),
+                Text = x.ModuleName
+            }).ToList();
+
+            var tests = _context.Test.Select(x => new SelectListItem()
+            {
+                Value = x.TestId.ToString(),
+                Text = x.DateOfTest + " " + x.City.CityName
+            }).ToList();
+
+            var viewModel = new AddCandidateViewModel() { SelectedModuleIds = new int?[] { null, null, null }, Modules = modules, Tests = tests };
+            return viewModel;
+        }
+    }
+}
---
+++ PRIS.WEB/ViewModels/CandidateViewModels/AddCandidateViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PRIS.WEB.Models;
+
+namespace PRIS.WEB.ViewModels.CandidateViewModels
+{
+    public class AddCandidateViewModel
+    {
+        [Required(ErrorMessage = "Įveskite...")]
+        [Display(Name = "Vardas")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Įveskite...")]
+        [Display(Name = "Pavardė")]
+        public string Lastname { get; set; }
+        [Required(ErrorMessage = "Įveskite...")]
+        [Display(Name = "El. Paštas")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Įveskite...")]
+        [Display(Name = "Telefonas")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Lytis")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Įveskite...")]
+        [Display(Name = "Testas")]
+        public int TestId { get; set; }
+
+        [Display(Name = "Komentaras")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Mokymosi programos")]
+        public int?[] SelectedModuleIds { get; set; }
+
+        public List<SelectListItem> Modules { get; set; }
+
+        public List<SelectListItem> Tests { get; set; }
+    }
+}
